c239154248e1afef87dea4221541a692f770de53
Julian Kelabora <jkelabora@dius.com.au>
2013-01-19 20:07:45 +1100

    split server code into per-message end-points, some re-factoring

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 A lightweight webserver which communicates via a simple message queue with a Raspberry Pi LED-strip control script - designed as an easy to use build pipeline monitor.
 
 ```
-$ brew install beanstalkd
-$ beanstalkd -l 127.0.0.1 -p 14711
+tar zxvf beanstalkd-1.8.tar.gz
+cd beanstalkd-1.8/
+make
 
-raspberry-pipeline $ sudo python webserver.py 
+beanstalkd -l 127.0.0.1 -p 14711 &
+sudo python webserver.py &
+python lights-controller.py &
+(fg ^C to kill)
 
-raspberry-pipeline $ python lights-controller.py 
+http://localhost:3142/start_build.html
 
-http://localhost:666/index.html?key=value
-http://localhost:666/index.html?key=value&debug=true
+http://localhost:3142/update.html?update=true&seg_1=green&seg_2=white_pulse&seg_3=red&seg_4=blue_pulse&seg_5=red
+http://localhost:3142/update.html?update=true&seg_1=green&seg_2=white_pulse&seg_3=red&seg_4=blue_pulse&seg_5=red&debug=true
+
+http://localhost:3142/all_off.html
 ```
--- webserver.py
+++ webserver.py
@@ -14,78 +14,74 @@ def value_for(key, source):
     # print parse_qsl(urlparse(url)[4])
     # [('foo', 'bar'), ('key', 'val')] --> a list of tuples
 
-def determine_message(query):
-    # all_off=true -> ignore all other params   
-    # ============
-    if value_for('all_off', query): 
-        return 'all_off'
+
+default_brightness = 1.0 # 0.0 -> 1.0
+default_led_count = 32
+default_segment_count = 5
+
+def segment_count(query):
+    return value_for('segment_count', query) if value_for('segment_count', query) else default_segment_count
+
+def led_start_offset(query):
+    return default_led_count % segment_count(query)
+
+def create_start_build_message(query):
+    return 'start_build:{0}:{1}:{2}'.format(led_start_offset(query), default_led_count, default_brightness)
+
+def create_update_message(query):
+    # http://raspberrypi:3142/update.html?update=true&seg_1=green&seg_2=white_pulse&seg_3=red&seg_4=blue_pulse&seg_5=white&debug=true
+    # return a message with segment directives ordered by segment index if possible
+    segment_width = default_led_count / segment_count(query)
+    message = "update:{0}:{1}:{2}:{3}".format(led_start_offset(query), segment_count(query), segment_width, default_brightness)
+    for i in range(segment_count(query)):
+        led_idx = i + 1
+        led_directive = value_for('seg_{0}'.format(led_idx), query)
+        if led_directive == None:
+            print "with {0} segments, expected a 'seg_{1}=' key".format(segment_count(query), led_idx)
+            return None
+        else:
+            colour = re.search(r"\b(green|blue|white|red)(_pulse)?\b", led_directive)
+            if colour:
+                print 'setting seg_{0} to {1}'.format(led_idx, colour.group())
+                message += ":{0}".format(colour.group())
+            else:
+                print "valid colour values are green|blue|white|red followed by an optional '_pulse' suffix, found {0} for seg_{1}".format(led_directive, led_idx)
+                return None
+    return message
+
+def handle_message(message, resp, debug_mode=False):
+    output = ''
+    if debug_mode:
+        output = "DEBUG: message posted: 'content' => '{0}'".format(message)
     else:
-        default_brightness = 1.0 # 0.0 -> 1.0
-        default_led_count = 32
-        default_segment_count = 5
-        segment_count = value_for('segment_count', query) if value_for('segment_count', query) else default_segment_count
-        led_start_offset = default_led_count % segment_count
-
-        # ============
-        # start_build=true -> respect defaults, ignore other params
-        if value_for('start_build', query):
-            start_led_index = led_start_offset
-            end_led_index = default_led_count
-            return 'start_build:{0}:{1}:{2}'.format(start_led_index, end_led_index, default_brightness)
-        
-        # ============
-        # http://localhost:3142/index.html?update=true&seg_1=green&seg_2=white_pulse&seg_3=red&seg_4=blue_pulse&seg_5=white
-        if value_for('update', query):
-
-            # return a message with segment directives ordered by segment index if possible
-            segment_width = default_led_count / segment_count
-            start_led_index = led_start_offset
-            message = "update:{0}:{1}:{2}:{3}".format(start_led_index, segment_count, segment_width, default_brightness)
-            for i in range(segment_count):
-                led_idx = i + 1
-                led_directive = value_for('seg_{0}'.format(led_idx), query)
-                if led_directive == None:
-                    print "with {0} segments, expected a 'seg_{1}=' key".format(segment_count, led_idx)
-                    return None
-                else:
-                    colour = re.search(r"\b(green|blue|white|red)(_pulse)?\b", led_directive)
-                    if colour:
-                        print 'setting seg_{0} to {1}'.format(led_idx, colour.group())
-                        message += ":{0}".format(colour.group())
-                    else:
-                        print "valid colour values are green|blue|white|red followed by an optional '_pulse' suffix, found {0} for seg_{1}".format(led_directive, led_idx)
-                        return None
-            return message
-        return None
+        beanstalk = beanstalkc.Connection(host='localhost', port=14711, parse_yaml=False)
+        msg_id = beanstalk.put(message)
+        output = "INFO: message posted: 'id':'content' => '{0}':'{1}'".format(msg_id, message)
+    print output
+    resp.send_response(200)
+    resp.send_header('Content-type', 'text/html')
+    resp.end_headers()
+    resp.wfile.write("<html><b>All good.. Posted message: '{0}'</b></html>".format(output))
 
 class LightSwitch(BaseHTTPRequestHandler):
-
-    # todo: split this out into more entry points
     def do_GET(self):
-        if re.search('.html', self.path):
+        url = self.path
+        parsed_query = urlparse(url).query.lower()
+        debug_mode = re.search('debug', parsed_query)
+        if re.search('all_off.html', url):
+            handle_message('all_off', self, debug_mode)
 
-            parsed = urlparse(self.path).query.lower()
-            message = determine_message(parsed)
+        elif re.search('start_build.html', url):
+            handle_message(create_start_build_message(parsed_query), self, debug_mode)
 
-            if re.search('debug', parsed):
-                print "DEBUG: message posted: 'content' => '{0}'".format(message)
-            elif(message):
-                beanstalk = beanstalkc.Connection(host='localhost', port=14711, parse_yaml=False)
-                msg_id = beanstalk.put(message)
-                print "INFO: message posted: 'id':'content' => '{0}':'{1}'".format(msg_id, message)
+        elif re.search('update.html', url):
+            message = create_update_message(parsed_query)
+            if message: 
+                handle_message(message, self, debug_mode)
             else:
                 self.send_error(400, 'Bad request')
-                return
-
-            self.send_response(200)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
-
-            self.wfile.write("<html><b>All good..</b></html>")
-            return
         else:
             self.send_error(404, 'Path not found')
-            return
         return
 
 def main():
