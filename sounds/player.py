8b7f02d87f7cc9c6cca2714d5b091dd51d38302d
Julian Kelabora <j.kelabora@diuscomputing.com.au>
2013-03-23 11:45:51 +1100

    kill off any currently playing sounds before playing a new one

--- sounds/player.py
+++ sounds/player.py
@@ -2,6 +2,8 @@ import os
 from random import randrange
 import glob
 import logging
+import subprocess
+import signal
 
 log = logging.getLogger()
 
@@ -17,8 +19,15 @@ class Player:
         self.play_this_thing(self.randomly_choose_mp3_in_sub_directory("failure"))
 
     def play_this_thing(self, filename):
+        self.kill_off_any_currently_playing_sounds
         log.info("playing {0}...".format(filename))
         os.system("mpg321 {0} &".format(filename))
+
+    def kill_off_any_currently_playing_sounds(self):
+        proc = subprocess.Popen(["pgrep", 'mpg321'], stdout=subprocess.PIPE)
+        for pid in proc.stdout:
+            log.info("killing off mpg321 process with PID {0}...".format(pid))
+            os.kill(int(pid), signal.SIGTERM) # there are other variants if this doesn't do the trick
 
     def randomly_choose_mp3_in_sub_directory(self, sub_directory):
         directory = "{0}/sounds/{1}/".format(os.environ['RPI_HOME'], sub_directory)
