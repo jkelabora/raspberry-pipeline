da5b27dcb0b52e1c63be6b5cc68bf97de15b30fd
Julian Kelabora <jkelabora@dius.com.au>
2013-07-11 14:39:47 +1000

    try again

--- lights_controller.py
+++ lights_controller.py
@@ -5,6 +5,7 @@ import os
 import Queue
 from time import sleep
 import logging
+import UnrecognisedDirective
 
 from queue_readers.aws_sqs import PollSQSWorker
 from message_translators.jenkins_translator import JenkinsMessageTranslator
