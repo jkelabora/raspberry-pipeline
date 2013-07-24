774f0db1291c34d5d74744a940848ee11b25aa97
Julian Kelabora <jkelabora@dius.com.au>
2013-07-24 16:19:14 +1000

    try and stop reporting threads silently dying

--- reporters/state_reporter.py
+++ reporters/state_reporter.py
@@ -7,6 +7,8 @@ import os
 import urllib2
 import json
 
+log = logging.getLogger()
+
 class StateReporter(threading.Thread):
 
   def __init__(self, reporter_q):
@@ -14,28 +16,28 @@ class StateReporter(threading.Thread):
     self.daemon = True # so that this thread get killed when the main thread does
 
     self.reporter_q = reporter_q
-    logging.getLogger().info('new pipeline(s) state reporter thread started..')
+    log.info('new pipeline(s) state reporter thread started..')
 
   def run(self):
     while True:
       sleep(10.0)
-      logging.getLogger().info("getting pipeline(s) status...")
+      log.info("getting pipeline(s) status...")
 
       try:
         report = self.reporter_q.get_nowait() # this will normally throw Queue.Empty
-      except Queue.Empty:
-        pass
+      except Queue.Empty: pass
 
       if report is not None:
-        logging.getLogger().info("posting to {0} this status: {1} ".format(os.environ['REPORTING_ENDPOINT'], report))
+        log.info("posting to {0} this status: {1}".format(os.environ['REPORTING_ENDPOINT'], report))
 
         req = urllib2.Request(os.environ['REPORTING_ENDPOINT'])
         req.add_header('Content-Type', 'application/json')
-        response = urllib2.urlopen(req, json.dumps(report))
+        try:
+          urllib2.urlopen(req, json.dumps(report))
+        except: log.error("problem making status report request..!")
 
         # delete all messages
         while self.reporter_q.empty() is False:
           try:
             self.reporter_q.get_nowait()
-          except Queue.Empty:
-            pass
+          except Queue.Empty: pass
